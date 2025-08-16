2ed8aabf14f2c4fb1e4c1740a7f2bb88fbbf11d6
LIUK-BJ <201204liu@sina.cn>
2025-08-16 12:58:48 +0800

    Create main.py
    俗称新建文件夹
    这是程序主文件，目前只实现了窗口标题和欢迎文字

---
+++ main.py
@@ -0,0 +1,6 @@
+import tkinter as tk #导入tkinter GUI库
+Mainwindow=tk.Tk()
+Mainwindow.title("KsCracker V0.0.1_Beta")#设置窗口标题
+Header=tk.Label(Mainwindow,text="Welcome to KsCracker！",font=("宋体",25))#标题文字 "Welcome to Kscracker！"
+Header.grid()#定位
+Mainwindow.mainloop()
